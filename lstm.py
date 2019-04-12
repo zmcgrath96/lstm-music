3221cae53fb2aa363baffd33d05f22c650316f8a
Zachary McGrath <zacharymcgrath@Zacharys-MacBook-Pro.local>
2019-04-12 12:13:50 -0500

    addes lstm and lstm_cell files

---
+++ lstm.py
@@ -0,0 +1,5 @@
+import numpy as np 
+from lstm_cell import cell
+
+class lstm():
+	print('howdy i am an lstm')
---
+++ lstm_cell.py
@@ -0,0 +1,5 @@
+import numpy as np 
+from activation_funcs import *
+
+class cell():
+	print('i am a lsmt cell')
