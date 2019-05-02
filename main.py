fb94a5d38501c4343f9fa591b744015fbb300262
Kevin <kevin_ray@ku.edu>
2019-05-02 13:56:57 -0500

    Add nn and outline for training

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 from lstm import musicLSTM
+from nn import musicNN
 import numpy as np
 import sys
 import pickle
 
 def main(args):
 	if '-t' in args[0]:
-		inputs = pickle.load(open('pickle/classical_inputs', 'rb'))
-		outputs = pickle.load(open('pickle/classical_outputs', 'rb'))
-		in_shape = (inputs.shape[1], inputs.shape[2])
-		out_size = outputs.shape[1]
+
+		# read in piana inputs
+		piano_inputs = pickle.load(open('pickle/piano_inputs', 'rb'))
+		# read in piano outputs
+		piano_outputs = pickle.load(open('pickle/piano_inputs', 'rb'))
+
+		# run piano through lstm
+		in_shape = (piano_inputs.shape[1], piano_inputs.shape[2])
+		out_size = piano_outputs.shape[1]
 		piano_lstm = musicLSTM(in_shape, out_size)
-		piano_lstm.train(inputs, outputs, 'model.h5')
+		piano_lstm.train(piano_inputs, piano_outputs, 'piano_model.h5')
+
+		# read in bass outputs
+		bass_outputs = pickle.load(open('pickle/bass_outputs', 'rb'))
+
+		# reshape piano inputs
+		piano_nn_inputs = piano_inputs.reshape((piano_inputs.shape[0] * piano_inputs.shape[1], piano_inputs.shape[2]))
+		nn_in_shape = (piano_nn_inputs.shape[1],)
+
+		# run bass through nn
+		bass_nn = musicNN(nn_in_shape, out_size)
+		bass_nn.train(piano_nn_inputs, bass_outputs, 'bass_model.h5')
+
+		# read in sax outputs
+		sax_outputs = pickle.load(open('pickle/sax_outputs', 'rb'))
+
+		# run sax through nn
+		sax_nn = musicNN(nn_in_shape, out_size)
+		sax_nn.train(piano_nn_inputs, sax_outputs, 'sax_model.h5')
+
 
 	elif '-g' in args[0]:
-		piano_lstm = musicLSTM((1,1), 5)
-		piano_lstm.predict('model.h5')
-	pass
+		
+		pass
 
 def train():
 	pass
